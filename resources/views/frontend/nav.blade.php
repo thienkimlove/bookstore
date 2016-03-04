efa6d655385bde0d7207f38778f95a6b55ad45fb
Tieu Ngao <thienkimlove@gmail.com>
2016-03-04 11:05:17 -0500

    fix

--- resources/views/frontend/nav.blade.php
+++ resources/views/frontend/nav.blade.php
@@ -7,7 +7,7 @@
                 <a href="{{url('/')}}" itemprop="url"><span itemprop="name">Home</span></a>
             </li>
 
-             @foreach ($categories as $category)
+             @foreach ($categories->splice(0, 7) as $category)
             <li id="menu-item-{{$category->id}}"
                 class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-58">
                 <a href="{{url($category->slug)}}" itemprop="url"><span itemprop="name">{{$category->name}}</span></a>
@@ -15,14 +15,8 @@
              @endforeach
             <li id="menu-item-57"
                 class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-57">
-                <a href="http://sciencebasic.com/uncategorized" itemprop="url"><span itemprop="name">General</span></a>
+                <a href="http://sciencebasic.com/uncategorized" itemprop="url"><span itemprop="name">More >></span></a>
                 <ul class="sub-menu">
-                    <li id="menu-item-58"
-                        class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13">
-                        <a href="{{url('/')}}" itemprop="url">
-                            <span itemprop="name">Home</span>
-                        </a>
-                    </li>
                     @foreach ($categories as $category)
                         <li id="menu-item-{{$category->id + 58}}"
                             class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-13">
