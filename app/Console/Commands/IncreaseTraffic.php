8937d5e92f739a4d91cde0d139ac66be052ddf0b
Quan Do <do.manh.quan@vsi-international.com>
2016-03-24 12:37:07 +0700

    test

--- app/Console/Commands/IncreaseTraffic.php
+++ app/Console/Commands/IncreaseTraffic.php
@@ -6,6 +6,7 @@ use App\Crawlers\Main;
 use App\Crawlers\ProxyChecker;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
+use Symfony\Component\DomCrawler\Crawler;
 
 class IncreaseTraffic extends Command
 {
@@ -33,6 +34,51 @@ class IncreaseTraffic extends Command
         parent::__construct();
     }
 
+    /*
+     * Crawler through proxy
+     */
+
+    public function throughProxy($url, $proxy)
+    {
+        //proxy = '218.161.34.107:8080';
+       return Main::crawlerLink($url,[ 'proxy' => $proxy, 'debug' => true ]);
+    }
+
+    /*
+     * Get Amazon Review in Frame Var Javascript
+     */
+    public function getAmazonReviews($url)
+    {
+        $output = Main::crawlerLink($url);
+        $output = explode('var iframeContent = "', $output);
+        $output1 = explode('";', $output[1]);
+        $crawler = new  Crawler(urldecode($output1[0]));
+        $reviews = [
+            'sources' => [],
+            'texts' => []
+        ];
+        $sourceHtmls = $crawler->filter('div.content > h3.productDescriptionSource');
+
+        if (iterator_count($sourceHtmls) > 0) {
+            foreach ($sourceHtmls as $sourceHtml) {
+                $temp = new Crawler($sourceHtml);
+                $reviews['sources'][] = $temp->text();
+            }
+        }
+
+        $textHtmls = $crawler->filter('div.content > div.productDescriptionWrapper');
+
+        if (iterator_count($textHtmls) > 0) {
+            foreach ($textHtmls as $textHtml) {
+                $temp = new Crawler($textHtml);
+                $reviews['texts'][] = $temp->text();
+            }
+        }
+
+        return $reviews;
+
+    }
+
     /**
      * Execute the console command.
      *
@@ -40,8 +86,9 @@ class IncreaseTraffic extends Command
      */
     public function handle()
     {
-        $url = 'http://medicinebooklet.com';
-        $response = Main::crawlerLink($url,[ 'proxy' => '218.161.34.107:8080', 'debug' => true ]);
-        $this->line('ooke');
+
+
+        $url = "http://www.amazon.com/gp/product/0767922476/ref=s9_simh_gw_g14_i3_r?ie=UTF8&fpl=fresh&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-1&pf_rd_r=1WPRC154AFQT2JQMSF4C&pf_rd_t=36701&pf_rd_p=2437869742&pf_rd_i=desktop";
+
     }
 }
